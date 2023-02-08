de4a76bfdc2ff20d707dd7232062f73db2a6bf6e
Melissa Beltrán <72474876+MelBelJ@users.noreply.github.com>
2023-02-08 14:30:48 -0600

    first commit

---
+++ StarWars/app/src/main/java/example/starwars/iest/edu/mx/MainActivity.kt
@@ -0,0 +1,11 @@
+package example.starwars.iest.edu.mx
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
